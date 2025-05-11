49ca15d2046e94b3c96386646dd250d201e0dc5b
leonardo <leo-maran1@hotmail.com>
2025-05-11 19:32:02 -0300

    fix: MIME

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/karina-vanco',
+  build: {
+    rollupOptions: {
+      output: {
+        format: 'es'
+      }
+    }
+  },
   plugins: [
     vue(),
     vueDevTools(),
