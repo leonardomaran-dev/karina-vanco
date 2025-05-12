380a969e8bc1497d1206eb5c2fbd10992b791001
leonardo <leo-maran1@hotmail.com>
2025-05-11 22:27:19 -0300

    ee

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,31 @@ import Hero from "@/components/Hero.vue";
 import AboutMe from "@/components/AboutMe.vue";
 import Services from "@/components/Services.vue";
 import FlashMessage from "../components/ui/FlashMessage.vue";
+
+import { ref, onMounted } from "vue";
+import FlashMessage from "@/components/ui/FlashMessage.vue";
+
+const flashMessage = ref({
+  show: false,
+  type: "success",
+  title: "",
+  message: "",
+});
+
+function hideFlashMessage() {
+  flashMessage.value.show = false;
+  localStorage.removeItem("flashMessage");
+}
+
+onMounted(() => {
+  const savedFlashMessage = localStorage.getItem("flashMessage");
+  if (savedFlashMessage) {
+    flashMessage.value = JSON.parse(savedFlashMessage);
+    setTimeout(() => {
+      hideFlashMessage();
+    }, 5000);
+  }
+});
 </script>
 
 <template>
@@ -20,30 +45,3 @@ import FlashMessage from "../components/ui/FlashMessage.vue";
     <Services />
   </div>
 </template>
-
-<script setup>
-import { ref, onMounted } from 'vue';
-import FlashMessage from "@/components/ui/FlashMessage.vue";
-
-const flashMessage = ref({
-  show: false,
-  type: "success",
-  title: "",
-  message: "",
-});
-
-function hideFlashMessage() {
-  flashMessage.value.show = false;
-  localStorage.removeItem('flashMessage');
-}
-
-onMounted(() => {
-  const savedFlashMessage = localStorage.getItem('flashMessage');
-  if (savedFlashMessage) {
-    flashMessage.value = JSON.parse(savedFlashMessage);
-    setTimeout(() => {
-      hideFlashMessage();
-    }, 5000);
-  }
-});
-</script>
